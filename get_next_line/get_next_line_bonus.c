0742ed68611e999a53ac2615bdc8419e370bc915
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-01-16 19:02:48 +0900

    Update get_next_line_bonus.c

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_line(char *str)
 {
