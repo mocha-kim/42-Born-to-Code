bc218db7b6099b7d2bc7dc32d97a1d079538f2f7
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-05-13 15:57:59 +0900

    cub3d norm

--- 2CIRCLE/cub3d/bonus/sprite2_bonus.c
+++ 2CIRCLE/cub3d/bonus/sprite2_bonus.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sunhkim <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/12 17:40:28 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/05/12 17:40:29 by sunhkim          ###   ########.fr       */
+/*   Created: 2021/04/28 19:56:24 by sunhkim           #+#    #+#             */
+/*   Updated: 2021/04/28 19:56:40 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,29 @@ void	calc_sprite_pos(t_sprt_line *sprt, int *order, t_info *info, int i)
 	sprt->x = info->sprite[order[i]].x - info->pos_x;
 	sprt->y = info->sprite[order[i]].y - info->pos_y;
 	inv_det = 1.0 / (info->pln_x * info->dir_y - info->dir_x * info->pln_y);
-	sprt->trns_x = inv_det * (info->dir_y * sprt->x - info->dir_x * sprt->y);
-	sprt->trns_y = inv_det * (-info->pln_y * sprt->x + info->pln_x * sprt->y);
-	sprt->scrn_x = (int)((info->conf.win_width / 2)
-				* (1 + sprt->trns_x / sprt->trns_y));
+	sprt->trans_x = inv_det * (info->dir_y * sprt->x - info->dir_x * sprt->y);
+	sprt->trans_y = inv_det * (-info->pln_y * sprt->x + info->pln_x * sprt->y);
+	sprt->screen_x = (int)((info->conf.win_width / 2)
+				* (1 + sprt->trans_x / sprt->trans_y));
 }
 
 void	calc_sprite_line(t_sprt_line *sprt, t_info *info)
 {
-	sprt->v_ms_screen = (int)(V_MOVE / sprt->trns_y);
-	sprt->height = (int)fabs((info->conf.win_height / sprt->trns_y) / V_DIV);
+	sprt->v_move_scrn = (int)(V_MOVE / sprt->trans_y);
+	sprt->height = (int)fabs((info->conf.win_height / sprt->trans_y) / V_DIV);
 	sprt->draw_strt_y = -sprt->height / 2 + info->conf.win_height
-						/ 2 + sprt->v_ms_screen;
+						/ 2 + sprt->v_move_scrn;
 	if (sprt->draw_strt_y < 0)
 		sprt->draw_strt_y = 0;
 	sprt->draw_end_y = sprt->height / 2 + info->conf.win_height
-						/ 2 + sprt->v_ms_screen;
+						/ 2 + sprt->v_move_scrn;
 	if (sprt->draw_end_y >= info->conf.win_height)
 		sprt->draw_end_y = info->conf.win_height - 1;
-	sprt->width = (int)fabs((info->conf.win_height / sprt->trns_y) / U_DIV);
-	sprt->draw_strt_x = -sprt->width / 2 + sprt->scrn_x;
+	sprt->width = (int)fabs((info->conf.win_height / sprt->trans_y) / U_DIV);
+	sprt->draw_strt_x = -sprt->width / 2 + sprt->screen_x;
 	if (sprt->draw_strt_x < 0)
 		sprt->draw_strt_x = 0;
-	sprt->draw_end_x = sprt->width / 2 + sprt->scrn_x;
+	sprt->draw_end_x = sprt->width / 2 + sprt->screen_x;
 	if (sprt->draw_end_x >= info->conf.win_width)
 		sprt->draw_end_x = info->conf.win_width - 1;
 }
@@ -55,15 +55,15 @@ void	coord_sprite_tex(t_info *info, int *order, t_sprt_line *sprt, int i)
 	stripe = sprt->draw_strt_x - 1;
 	while (++stripe < sprt->draw_end_x)
 	{
-		sprt->tex_x = (int)((256 * (stripe - (-sprt->width / 2 + sprt->scrn_x))
-					* TEX_WIDTH / sprt->width) / 256);
-		if (sprt->trns_y > 0 && stripe > 0 && stripe < info->conf.win_width
-					&& sprt->trns_y < info->z_buf[stripe])
+		sprt->tex_x = (int)((256 * (stripe - (-sprt->width /
+					2 + sprt->screen_x)) * TEX_WIDTH / sprt->width) / 256);
+		if (sprt->trans_y > 0 && stripe > 0 && stripe < info->conf.win_width
+					&& sprt->trans_y < info->z_buf[stripe])
 		{
 			y = sprt->draw_strt_y - 1;
 			while (++y < sprt->draw_end_y)
 			{
-				d = (y - sprt->v_ms_screen) * 256 - info->conf.win_height * 128
+				d = (y - sprt->v_move_scrn) * 256 - info->conf.win_height * 128
 					+ sprt->height * 128;
 				sprt->tex_y = ((d * TEX_HEIGHT) / sprt->height) / 256;
 				sprt->color = info->texture[info->sprite[order[i]].texture]
