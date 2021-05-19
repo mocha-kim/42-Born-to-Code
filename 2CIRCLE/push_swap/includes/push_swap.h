c6c6223625ea2bdc7e12887b8de8bb18c0d91843
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-05-19 21:08:53 +0900

    push_swap

---
+++ 2CIRCLE/push_swap/includes/push_swap.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 21:05:14 by sunhkim           #+#    #+#             */
+/*   Updated: 2021/05/19 21:08:37 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+/*
+** sa : swap a - swap the first 2 elements at the top of stack a. Do nothing if there is only one or no elements).
+** sb : swap b - swap the first 2 elements at the top of stack b. Do nothing if there is only one or no elements).
+** ss : sa and sb at the same time.
+*/
+
+/*
+** pa : push a - take the first element at the top of b and put it at the top of a. Do nothing if b is empty.
+** pb : push b - take the first element at the top of a and put it at the top of b. Do nothing if a is empty.
+*/
+
+/*
+** ra : rotate a - shift up all elements of stack a by 1. The first element becomes the last one.
+** rb : rotate b - shift up all elements of stack b by 1. The first element becomes the last one.
+** rr : ra and rb at the same time.
+*/
+
+/*
+** rra : reverse rotate a - shift down all elements of stack a by 1. The last element becomes the first one.
+** rrb : reverse rotate b - shift down all elements of stack b by 1. The last element becomes the first one.
+** rrr : rra and rrb at the same time.
+*/
+
+#endif
