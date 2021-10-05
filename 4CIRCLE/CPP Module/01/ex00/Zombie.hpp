a5f5a9a5bf85e6eaa1d6119522b613a1264e57ab
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-05 19:20:47 +0900

    [CPP Module 01] init

---
+++ 4CIRCLE/CPP Module/01/ex00/Zombie.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/05 19:01:38 by sunhkim           #+#    #+#             */
+/*   Updated: 2021/10/05 19:11:39 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+private:
+	std::string	name;
+
+public:
+	Zombie();
+	~Zombie();
+
+	void	announce();
+};
+
+#endif
