4253c6111d0a56f3b4c395da4f8acd21617b0180
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-07 00:52:04 +0900

    [CPP Module 01] ex04

--- 4CIRCLE/CPP Module/01/ex04/main.cpp
+++ 4CIRCLE/CPP Module/01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 21:56:09 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/10/07 00:26:45 by sunhkim          ###   ########.fr       */
+/*   Updated: 2021/10/07 00:51:44 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char* argv[])
 	CppSed	sed;
 	if (argc != 4)
 	{
-		std::cout << "Error: Invailed argument number" << std::endl;
+		std::cout << "Error: Invaild argument number" << std::endl;
 		return (-1);
 	}
 	if (!sed.setString(argv[2], argv[3]))
