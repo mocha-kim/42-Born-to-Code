00a3a91f6d3bc84c7286d13dbc748caa3ff41fb9
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-06 20:16:18 +0900

    [CPP Module 01] ex00

--- 4CIRCLE/CPP Module/01/ex00/Zombie.hpp
+++ 4CIRCLE/CPP Module/01/ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 19:01:38 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/10/06 20:00:35 by sunhkim          ###   ########.fr       */
+/*   Updated: 2021/10/06 20:15:58 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ private:
 	void	_prefix();
 
 public:
+	Zombie();
 	Zombie(std::string name);
 	~Zombie();
 
