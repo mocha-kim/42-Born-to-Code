0d81e453a4ec020fe3f9ed0cbd381dd4ab342d02
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-06 20:37:26 +0900

    [CPP Module 01] ex02

---
+++ 4CIRCLE/CPP Module/01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 20:30:29 by sunhkim           #+#    #+#             */
+/*   Updated: 2021/10/06 20:37:02 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "===== TEST1 : print address =====" << std::endl;
+	std::cout << "string: " << &str << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	std::cout << "===== TEST2 : print string ======" << std::endl;
+	std::cout << "string: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
