1608a7fe33d4d5fa67830edbadee57a5b811c79d
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-12 19:22:11 +0900

    [CPP Module 03] ex02

---
+++ 4CIRCLE/CPP Module/03/ex02/ClapTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 16:45:56 by sunhkim           #+#    #+#             */
+/*   Updated: 2021/10/12 18:32:15 by sunhkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap
+{
+protected:
+	std::string	name;
+	int			hitpoints;
+	int			energy;
+	int			damage;
+
+	virtual void	_prefix();
+
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &copy);
+	~ClapTrap();
+
+	ClapTrap		&operator=(const ClapTrap &op);
+
+	void			attack(const std::string &target);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+	virtual void	debug();
+};
+
+#endif
