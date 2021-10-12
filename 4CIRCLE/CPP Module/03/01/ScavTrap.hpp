754ba7c7ad8969f292760a05dcd54c8dc7b9e9c6
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-12 18:54:30 +0900

    [CPP Module 03] ex01

--- 4CIRCLE/CPP Module/03/01/ScavTrap.hpp
+++ 4CIRCLE/CPP Module/03/01/ScavTrap.hpp
@@ -6,12 +6,12 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:22:58 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/10/12 17:55:30 by sunhkim          ###   ########.fr       */
+/*   Updated: 2021/10/12 18:54:13 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAV_TRAP_HPP
-#define SCAV_TRAP_HPP
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
 
 #include "ClapTrap.hpp"
 
