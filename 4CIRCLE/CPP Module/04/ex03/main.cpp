16a271065e49f0162721ae07dbef24f062eed511
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-19 17:23:16 +0900

    [CPP Module 04] ex03

--- 4CIRCLE/CPP Module/04/ex03/main.cpp
+++ 4CIRCLE/CPP Module/04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/13 20:34:15 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/10/19 16:55:13 by sunhkim          ###   ########.fr       */
+/*   Updated: 2021/10/19 17:15:07 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ int main()
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
 	AMateria* tmp;
+
+	ICharacter* me = new Character("me");
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
@@ -32,8 +33,24 @@ int main()
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
-	delete bob;
+	std::cout << std::endl;
+
 	delete me;
+
+	Character original = Character("original");
+	tmp = src->createMateria("ice");
+	original.equip(tmp);
+	tmp = src->createMateria("cure");
+	original.equip(tmp);
+	Character copy = original;
+	copy.unequip(0);
+	copy.use(0, *bob);
+	copy.use(1, *bob);
+	original.use(0, *bob);
+	original.use(1, *bob);
+
+	delete bob;
 	delete src;
+	
 	return 0;
 }
