0ee37f45dfc6bc604d500b54f4dc2be346ade75e
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-10-25 14:33:42 +0900

    [CPP Module 07] ex02

--- 4CIRCLE/CPP Module/07/ex02/main.cpp
+++ 4CIRCLE/CPP Module/07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sunhkim <sunhkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 21:32:09 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/10/23 22:11:37 by sunhkim          ###   ########.fr       */
+/*   Updated: 2021/10/25 14:30:42 by sunhkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <string>
 
 #include "Array.hpp"
+
+#define MAX_VAL 750
 
 int	main()
 {
@@ -73,6 +75,54 @@ int	main()
 	{
 		std::cerr << e.what() << std::endl;
 	}
+	std::cout << std::endl;
+
+	std::cout << "=================Test5: mirror=================" << std::endl;
+	Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;
 
 	return 0;
 }
